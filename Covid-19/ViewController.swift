2b46ac7c3abe386b27aa67b72087d28b917ca69a
Matheus Holanda <matheus.albuquerque@agilecontent.com>
2020-03-23 14:58:36 -0300

    Git Ignore file added.

---
+++ Covid-19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid-19
+//
+//  Created by Matheus Holanda Pinheiro de Albuquerque on 23/03/20.
+//  Copyright © 2020 Matheus Holanda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
