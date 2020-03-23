56f4719413dcfe198bf0b8d52102403b573b7ac9
Matheus Holanda <matheus.albuquerque@agilecontent.com>
2020-03-23 17:31:00 -0300

    Worldwide cases services, model and view model added.

---
+++ Covid-19/Models/Cases.swift
@@ -0,0 +1,26 @@
+//
+//  Cases.swift
+//  Covid-19
+//
+//  Created by Matheus Holanda Pinheiro de Albuquerque on 23/03/20.
+//  Copyright © 2020 Matheus Holanda. All rights reserved.
+//
+
+import Foundation
+
+class Cases: Codable {
+    
+    var cases: Int?
+    var deaths: Int?
+    var recovered: Int?
+    var updated: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case cases, deaths, recovered, updated
+    }
+    
+    static func decode(from data: Data) -> Cases? {
+        let decoded = try? JSONDecoder().decode(Cases.self, from: data)
+        return decoded
+    }
+}
---
+++ Covid-19/Services/ApiClient.swift
@@ -0,0 +1,29 @@
+//
+//  ApiClient.swift
+//  Covid-19
+//
+//  Created by Matheus Holanda Pinheiro de Albuquerque on 23/03/20.
+//  Copyright © 2020 Matheus Holanda. All rights reserved.
+//
+
+import Foundation
+
+class ApiClient {
+
+    var baseURL = "https://corona.lmao.ninja/"
+        
+    static let shared = ApiClient()
+    
+    func fetchAllWorldwideCases(completion: @escaping (Result<Cases, Error>) -> ()) {
+        guard let url = URL(string: baseURL + "all/") else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let errorMessage = error {
+                print(errorMessage.localizedDescription)
+                completion(.failure(errorMessage))
+                return
+            } else if let currentData = data, let worldwideCases = Cases.decode(from: currentData) {
+                completion(.success(worldwideCases))
+            }
+            }.resume()
+    }
+}
